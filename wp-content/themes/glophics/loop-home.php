bf960a4be584426616b6b6598f0ea5617b6a8f84
alraif <alraif.glophics@gmail.com>
2020-02-07 09:21:52 +0800

    blog skeleton

--- wp-content/themes/glophics/loop-home.php
+++ wp-content/themes/glophics/loop-home.php
@@ -1,24 +1,15 @@
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<?php if ( is_front_page() ) { ?>
-			<h1 class="compname">What is <span>Lorem Ipsum</span>?</h1>
-		<?php } else { ?>
-			<h1 class="compname"><span><?php the_title(); ?></span></h1>
-			<?php if($post->post_content=="" && !is_page('sitemap')) { ?>
-				<p>We are still updating our website with contents. Please check back next time.</p>
-			<?php } ?>
-		<?php } ?>
-		<div class="entry-content">
+<?php
+    $args = array(
+        'post_type' => 'post'
+    );
 
-			<?php echo do_shortcode("[page_intro id='" . get_the_ID() . "']"); ?>
-
-			<?php the_content(); ?>
-
-			<?php if(is_page('sitemap')){ ?>
-				<ol class="sitemap"><?php wp_list_pages(array('title_li' => '')); ?></ol>
-			<?php } ?>
-			<!-- <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'twentyten' ), 'after' => '</div>' ) ); ?> -->
-			<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?> 
-		</div><!-- .entry-content -->
-	</div><!-- #post-## -->
-<?php endwhile; // end of the loop. ?>
+    $post_query = new WP_Query($args);
+	if($post_query->have_posts() ) {
+		while($post_query->have_posts() ) {
+			$post_query->the_post(); ?>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<p><?php the_content(); ?></p>
+			<?php
+		}
+	}
+?>
---
+++ wp-content/themes/glophics/single.php
@@ -0,0 +1,18 @@
+<?php
+@session_start();
+get_includes('head');
+get_includes('header');
+get_includes('banner');
+?>
+<main>
+	<div class="wrapper">
+        <?php
+        /* Run the loop to output the post.
+            * If you want to overload this in a child theme then include a file
+            * called loop-single.php and that will be used instead.
+            */
+        get_template_part( 'loop', 'single' );
+        ?>
+    </div>
+</main>
+<?php get_includes('footer');?>
